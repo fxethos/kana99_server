19015588d12a22498efaa035d868ab04b339dd30
priyan <cypher@pop-os.localdomain>
2021-08-23 14:41:09 +0530

    added deserialization

--- app/sc_controller/client/payout.js
+++ app/sc_controller/client/payout.js
@@ -3,7 +3,7 @@
 //npm run build:program-rust
 const data = require('./Accountdata.json')
 const path = require('path');
-const fs  = require('mz/fs')
+const fs = require('mz/fs')
 const solanaWeb3 = require('@solana/web3.js');
 const borsh = require('borsh')
 
@@ -15,28 +15,28 @@ const {
   getRpcUrl,
   newAccountWithLamports,
   createKeypairFromFile,
-} = require ('./utils')
+} = require('./utils')
 
 
-let destinationKey = new solanaWeb3.PublicKey("DeH2ULUKz8Hw33fmXU9a5ATf9cabk6cKUf7U9EJjuW5v")
+let destinationKey = new solanaWeb3.PublicKey("DAX5j7gUttmK6c73uqWgeH4UjbtwVQtmCS9VjNv1FEKC")
 
 const NETWORK = solanaWeb3.clusterApiUrl('devnet');
 
 /**
  * Connection to the network
  */
-let connection =  new solanaWeb3.Connection(NETWORK);
+let connection = new solanaWeb3.Connection(NETWORK);
 
 
 /**
  * Hello world's program id
  */
-let programId =   solanaWeb3.PublicKey;
+let programId = solanaWeb3.PublicKey;
 
 /**
  * The public key of the account we are saying hello to
  */
-let greetedPubkey =  solanaWeb3.PublicKey;
+let greetedPubkey = solanaWeb3.PublicKey;
 
 /**
  * Path to program files
@@ -60,93 +60,102 @@ const PROGRAM_KEYPAIR_PATH = path.join(PROGRAM_PATH, 'helloworld-keypair.json');
 /**
  * The state of a greeting account managed by the hello world program
  */
- const GreetingAccount = /** @class */ (function () {
-  function GreetingAccount(fields) {
-      if (fields === void 0) { fields = undefined; }
-      this.counter = 0;
-      if (fields) {
-          this.counter = fields.counter;
-      }
-  }
-  return GreetingAccount;
-}());
 
-const PayoutAmount = /** @class */ (function () {
-    function PayoutAmount(fields) {
-        if (fields === void 0) { fields = undefined; }
-        this.amount = 0;
-        if (fields) {
-            this.amount = fields.amount;
-        }
+
+const PayoutAmount = (function () {
+  function PayoutAmount(fields) {
+    if (fields === void 0) { fields = undefined; }
+    this.amount = 0;
+    this.datatype = 0;
+    if (fields) {
+      this.amount = fields.amount;
+      this.datatype = fields.datatype;
     }
-    return PayoutAmount;
+  }
+  return PayoutAmount;
 }());
 
-const GreetingSchema = new Map([
-  [GreetingAccount, {kind: 'struct', fields: [['counter', 'u32']]}],
-]);
 
 const PayoutSchema = new Map([
-    [PayoutAmount,{kind: 'struct', fields:[['amount', 'u64']]}],
-  ])
+  [PayoutAmount, { kind: 'struct', fields: [['amount', 'u64'], ['datatype', 'u64']] }],
+])
 
-  /**
- * The expected size of each greeting account.
- */
 
-   const GREETING_SIZE = borsh.serialize(
-    GreetingSchema,
-    new GreetingAccount(),
-  ).length;
+const ContestInstruction = (function () {
+  function ContestInstruction(fields) {
+    if (fields === void 0) { fields = undefined; }
+    this.contestids = 0;
+    this.datatype = 0
+    if (fields) {
+      this.contestids = fields.contestids;
+      this.datatype = fields.datatype;
+
+    }
+  }
+  return ContestInstruction;
+}());
+
+const ContestSchema = new Map([
+  [ContestInstruction, { kind: 'struct', fields: [['contestids', 'u64'], ['datatype', 'u64']] }],
+]);
+
+/**
+* The expected size of each greeting account.
+*/
+
+const GREETING_SIZE = borsh.serialize(
+  PayoutSchema,
+  new PayoutAmount(),
+).length;
 
 const PAYOUT_AMOUNT_SIZE = borsh.serialize(
-    PayoutSchema,
-    new PayoutAmount()
-  ).length
+  PayoutSchema,
+  new PayoutAmount()
+).length
 
-  /**
- * Establish a connection to the cluster
- */
+/**
+* Establish a connection to the cluster
+*/
 
-  const establishConnection = async ()=>{
-  
-    //console.log('establishConnection')
-    const rpcUrl = await getRpcUrl();
-    const connection = new solanaWeb3.Connection(rpcUrl, 'confirmed');
-    const version = await connection.getVersion();
-    console.log('Connection to cluster established:', rpcUrl, version);
-  }
+const establishConnection = async () => {
+
+  //console.log('establishConnection')
+  const rpcUrl = await getRpcUrl();
+  const connection = new solanaWeb3.Connection(rpcUrl, 'confirmed');
+  const version = await connection.getVersion();
+  console.log('Connection to cluster established:', rpcUrl, version);
+}
 
 
-  /**
- * Establish an account to pay for everything
- */
-const establishPayer= async () => {
+/**
+* Establish an account to pay for everything
+*/
+const establishPayer = async () => {
   let payer = await getPayer();
 
   //console.log('establishPayer')
 
-    let fees = 0;
-    if (!payer) {
-      const {feeCalculator} = await connection.getRecentBlockhash();
-  
-      // Calculate the cost to fund the greeter account
-      fees += await connection.getMinimumBalanceForRentExemption(GREETING_SIZE);
-  
-      // Calculate the cost of sending transactions
-      fees += feeCalculator.lamportsPerSignature * 100; // wag
-  
-      try {
-        // Get payer from cli config
-        payer = await getPayer();
-        
-      } catch (err) {
-        // Fund a new payer via airdrop
-        payer = await newAccountWithLamports(connection, fees);
-      }
-    }
+  let fees = 0;
+  if (!payer) {
+    const { feeCalculator } = await connection.getRecentBlockhash();
 
-    const lamports = await  connection.getBalance(payer.publicKey);
+    // Calculate the cost to fund the greeter account
+    fees += await connection.getMinimumBalanceForRentExemption(GREETING_SIZE);
+
+    // Calculate the cost of sending transactions
+    fees += feeCalculator.lamportsPerSignature * 100; // wag
+
+    try {
+      // Get payer from cli config
+      payer = await getPayer();
+
+    } catch (err) {
+      // Fund a new payer via airdrop
+      payer = await newAccountWithLamports(connection, fees);
+    }
+  }
+
+  const lamports = await connection.getBalance(payer.publicKey);
   if (lamports < fees) {
     // This should only happen when using cli config keypair
     const sig = await connection.requestAirdrop(
@@ -168,24 +177,24 @@ const establishPayer= async () => {
 /**
  * Check if the hello world BPF program has been deployed
  */
- const checkProgram = async ()=>{
+const checkProgram = async () => {
   //console.log('chkprogram')
   let payer = await getPayer();
 
-    // Read program id from keypair file
-    try {
-      const programKeypair = await createKeypairFromFile(PROGRAM_KEYPAIR_PATH);
-      //console.log(programKeypair)
+  // Read program id from keypair file
+  try {
+    const programKeypair = await createKeypairFromFile(PROGRAM_KEYPAIR_PATH);
+    //console.log(programKeypair)
 
-      programId =  programKeypair.publicKey;
-    } catch (err) {
-      const errMsg = err.message;
-      throw new Error(
-        `Failed to read program keypair at '${PROGRAM_KEYPAIR_PATH}' due to error: ${errMsg}. Program may need to be deployed with \`solana program deploy dist/program/helloworld.so\``,
-      );
-    }
+    programId = programKeypair.publicKey;
+  } catch (err) {
+    const errMsg = err.message;
+    throw new Error(
+      `Failed to read program keypair at '${PROGRAM_KEYPAIR_PATH}' due to error: ${errMsg}. Program may need to be deployed with \`solana program deploy dist/program/helloworld.so\``,
+    );
+  }
 
-    // Check if the program has been deployed
+  // Check if the program has been deployed
   const programInfo = await connection.getAccountInfo(programId);
   if (programInfo === null) {
     if (fs.existsSync(PROGRAM_SO_PATH)) {
@@ -210,109 +219,123 @@ const establishPayer= async () => {
 
   //console.log('greeted account' , greetedPubkey)
 
-    // Check if the greeting account has already been created
-    const greetedAccount = await connection.getAccountInfo(greetedPubkey);
-    if (greetedAccount === null) {
-      console.log(
-        'program owned account',
-        greetedPubkey.toBase58(),
-      );
-      const lamports = await connection.getMinimumBalanceForRentExemption(
-        GREETING_SIZE,
-      );
+  // Check if the greeting account has already been created
+  const greetedAccount = await connection.getAccountInfo(greetedPubkey);
+  if (greetedAccount === null) {
+    console.log(
+      'program owned account',
+      greetedPubkey.toBase58(),
+    );
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+      GREETING_SIZE,
+    );
 
-      const transaction = new solanaWeb3.Transaction().add(
-        solanaWeb3.SystemProgram.createAccountWithSeed({
-          fromPubkey: payer.publicKey,
-          basePubkey: payer.publicKey,
-          seed: GREETING_SEED,
-          newAccountPubkey: greetedPubkey,
-          lamports,
-          space: 64,
-          programId,
-        }),
-      );
-      await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [payer]);
-    }
+    const transaction = new solanaWeb3.Transaction().add(
+      solanaWeb3.SystemProgram.createAccountWithSeed({
+        fromPubkey: payer.publicKey,
+        basePubkey: payer.publicKey,
+        seed: GREETING_SEED,
+        newAccountPubkey: greetedPubkey,
+        lamports,
+        space: 16,
+        programId,
+      }),
+    );
+    await solanaWeb3.sendAndConfirmTransaction(connection, transaction, [payer]);
   }
+}
 
 var requiredAccounts = new Array();
 
-const logging = () =>{
-    console.log('started logging')
-    connection.onLogs(destinationKey,async function(logs,context){
-      if(logs.err==null){
+const logging = () => {
+  console.log('started logging')
+  connection.onLogs(destinationKey, async function (logs, context) {
+    // console.log(logs)
+    if (logs.err == null) {
 
-        let signature = logs.signature
+      let signature = logs.signature
 
-        let payer_pubkey =  await connection.getParsedConfirmedTransaction(signature);
-        let account = await payer_pubkey.transaction.message.instructions
+      let payer_pubkey = await connection.getParsedConfirmedTransaction(signature);
+      let account = await payer_pubkey.transaction.message.instructions
+      let accounts = await account[0].parsed.info.source
+      if (requiredAccounts.indexOf(accounts) == -1) {
+        requiredAccounts.push(accounts)
+        console.log(requiredAccounts)
 
-        let accounts = await account[0].parsed
-
-        requiredAccounts.push(accounts.info.source)
-            console.log(requiredAccounts)
-        
-    
+      }else {
+        console.log("Account is already present")
       }
-    })  
-    }
 
-  /**
- * Say hello
- */
+
+
+
+    }
+  })
+}
+
+/**
+* Say hello
+*/
 console.log("Program started")
-const sendPayouts = async  (req,res)=>{
+const sendPayouts = async (req, res) => {
   let payer = await getPayer();
 
   console.log('payouts from', greetedPubkey.toBase58());
-  for(let i=0 ; i<requiredAccounts.length; i++){
-        let destKey = requiredAccounts[i];
-        console.log("destination key" ,destKey)
-        const account_signer_destination = await new solanaWeb3.PublicKey(destKey);
-        let payout_Amount = new PayoutAmount()
-        payout_Amount.amount = 200
-        const instruction = new solanaWeb3.TransactionInstruction(
-              {keys: [
-                {pubkey : greetedPubkey,        isSigner : false,  isWritable: true},  
-                {pubkey : account_signer_destination,   isSigner : false, isWritable: true},
-              ],
-              programId,
-              data: Buffer.from(borsh.serialize(PayoutSchema,payout_Amount)),
-          // All instructions are hellos
-        });
-        await solanaWeb3.sendAndConfirmTransaction(
-              connection,
-              new solanaWeb3.Transaction().add(instruction),
-              [payer],
-                  {commitment: 'singleGossip', preflightCommitment: 'singleGossip',}
-        ).then(()=>{console.log('transaction made to '  +  destKey + 'and paid ' +  payout_Amount.amount  +  'lamports')}).catch((e)=>{console.log(e)});
+  for (let i = 0; i < requiredAccounts.length; i++) {
+    let destKey = requiredAccounts[i];
+    console.log("destination key", destKey)
+    const account_signer_destination = await new solanaWeb3.PublicKey(destKey);
+
+    let payout_Amount = new PayoutAmount()
+    payout_Amount.amount = 200
+    console.log(payout_Amount)
+    payout_Amount.datatype = 2
+    const instruction = new solanaWeb3.TransactionInstruction(
+      {
+        keys: [
+          { pubkey: greetedPubkey, isSigner: false, isWritable: true },
+          { pubkey: account_signer_destination, isSigner: false, isWritable: true },
+        ],
+        programId,
+        data: Buffer.from(borsh.serialize(PayoutSchema, payout_Amount)),
+        // All instructions are hellos
+      });
+    await solanaWeb3.sendAndConfirmTransaction(
+      connection,
+      new solanaWeb3.Transaction().add(instruction),
+      [payer],
+      { commitment: 'singleGossip', preflightCommitment: 'singleGossip', }
+    ).then(() => { console.log('transaction made to ' + destKey + 'and paid ' + payout_Amount.amount + 'lamports') }).catch((e) => { console.log(e) });
   }
-  
+
+}
+
+const reportContest = async () => {
+  console.log("reported greetings")
+  const accountInfo = await connection.getAccountInfo(greetedPubkey);
+  if (accountInfo === null) {
+    throw 'Error: cannot find the greeted account';
+  }
+  const recievedata = borsh.deserialize(
+
+    ContestSchema,
+    ContestInstruction,
+    accountInfo.data,
+  );
+  console.log(
+    greetedPubkey.toBase58(),
+    'contestid',
+    recievedata
+
+  );
 }
 
 
-module.exports = {sendPayouts,checkProgram,establishPayer,establishConnection,logging}
+
+
+
+module.exports = { sendPayouts, checkProgram, establishPayer, establishConnection, logging, reportContest }
 
 /**
  * Report the number of times the greeted account has been said hello to
  */
-// export async function reportGreetings(): Promise<void> {
-//   const accountInfo = await connection.getAccountInfo(greetedPubkey);
-//   if (accountInfo === null) {
-//     throw 'Error: cannot find the greeted account';
-//   }
-//   const greeting = borsh.deserialize(
-//     PayoutSchema,
-//     PayoutAmount,
-//     accountInfo.data,
-//   );
-//   console.log(
-//     greetedPubkey.toBase58(),
-//     'has been greeted',
-//     greeting.amount,
-//     'time(s)',
-//   );
-// }
-
-
--- app/sc_controller/program-rust/src/processor.rs
+++ app/sc_controller/program-rust/src/processor.rs
@@ -3,57 +3,81 @@ use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
     msg,
-    pubkey::Pubkey,
     program_error::ProgramError,
-
+    pubkey::Pubkey,
 };
 
-
-
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct Amount{
-    pub amount: u64,
+pub struct InstructionData {
+    pub revieveddata: u64,
+    pub datatype: u64,
 
 }
 
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct Amount {
+    pub amount: u64,
+    pub datatype: u64,
 
+}
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+pub struct ContestInstruction {
+    pub contestids: u64,
+    pub datatype: u64,
 
+}
 
 pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-
-
-   
     let accounts_iter = &mut accounts.iter();
 
-    let source_info = next_account_info(accounts_iter)?;
-    let destination_info = next_account_info(accounts_iter)?;
-
-    // The account must be owned by the program in order to modify its data
-    if source_info.owner != program_id {
-        msg!("Greeted account does not have the correct program id");
-        return Err(ProgramError::IncorrectProgramId);
-    }
-
-        
-    let message = Amount::try_from_slice(instruction_data).map_err(|err| {
+    let message = InstructionData::try_from_slice(instruction_data).map_err(|err| {
         msg!("Receiving message as string utf8 failed, {:?}", err);
-        ProgramError::InvalidInstructionData  
-      })?;
+        ProgramError::InvalidInstructionData
+    })?;
+  
+   // msg!("message {:?}", message);
+
      
+    if message.datatype == 1 {
+        msg!("Recieved request for payouts");
+        let source_info = next_account_info(accounts_iter)?;
+        let destination_info = next_account_info(accounts_iter)?;
+        let amount = message.revieveddata;
+        //The account must be owned by the program in order to modify its data
+        if source_info.owner != program_id {
+            msg!("Greeted account does not have the correct program id");
+            return Err(ProgramError::IncorrectProgramId);
+        };
 
-    // Withdraw five lamports from the source
-    **source_info.try_borrow_mut_lamports()? -= message.amount;
-    // Deposit five lamports into the destination
-    **destination_info.try_borrow_mut_lamports()? += message.amount;
+       
 
-    msg!("paid {:?} to {:?}", message.amount, destination_info.key);
+        // Withdraw five lamports from the source
+        **source_info.try_borrow_mut_lamports()? -= amount;
+        // Deposit five lamports into the destination
+        **destination_info.try_borrow_mut_lamports()? += amount;
+        msg!("paid {:?} to {:?}", amount, destination_info.key);
+        msg!("paid {:?} ",amount);
 
-    Ok(())
+    } else if message.datatype == 2{
+        let account = next_account_info(accounts_iter)?;
+        let contestid = message.revieveddata;
+        // let  greeting_account = ContestInstruction::try_from_slice(&account.data.borrow())?;
+        // greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+        msg!("contestid {:?}",contestid);
+        let data = &mut &mut account.data.borrow_mut();
+        msg!("Start save instruction into data");
+        data[..instruction_data.len()].copy_from_slice(&instruction_data);
+
+    };
 
    
+    
+
+    Ok(())
 }
+
 
